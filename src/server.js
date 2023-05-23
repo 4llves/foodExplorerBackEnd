4d56e8a29c9469e8d11381b58d317cf01ae7c973
Alves Jhonata <jhonatab4@gmail.com>
2023-05-23 00:31:57 -0300

    add AppError

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
+require('express-async-errors');
+
 const express = require('express');//importo
+
+const AppError = require('./utils/AppError');
 
 const routes = require("./routes");
 
@@ -6,6 +10,24 @@ const app = express();//inicio o express
 app.use(express.json());//padrão que receberá as info pelo corpo da req
 
 app.use(routes);
+
+app.use((error, req, res, next) => {
+  // verifica se a instacia do error vem de um AppError
+  // e retorna se vem um erro do lado do cliente
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  // verifica se a instacia do error vem de um AppError
+  // e retorna se vem um erro do lado do servidor
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+})
 
 const PORT = 3333;//crio o numero da porta
 app.listen(PORT, () => console.log(`Server ir running on Port ${PORT} 🚀`));//aqui vai ficar escutando a porta que informei
